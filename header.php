84383362a6947434cb2c061ea874d43298e644fe
Alexandre etienne <alex@etienne26.fr>
2024-10-08 19:00:28 +0200

    last update

--- header.php
+++ header.php
@@ -31,6 +31,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/contact.php">Contact</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/deconnexion.php">Acceuil</a>
+        </li>
       </ul>
     </div>
   </nav>
--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Inclure le header et la connexion à la base de données
 include 'header.php';
 include_once 'config/db.php';
